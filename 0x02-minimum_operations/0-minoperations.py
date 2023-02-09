270b1321f7f832099cfe4a318c321d52f1d3d005
Marvin Kurland <bugemarvin@outlook.com>
2023-02-09 22:44:34 +0300

    minimum number of operations needed to result in n H characters in the file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,11 +4,13 @@ function to find the min number occurence.
 '''
 
 
-def minOperations(n):
+def minOperations(n: int):
     '''
         minimum number of operations needed to result
         For n H characters in the file.
     '''
+    if n <= 1:
+        return 0
     count = 0
     while n > 1:
         f = 2
@@ -22,4 +24,3 @@ def minOperations(n):
             count += 1
             n /= f
     return count
-
