1d2b614b2f0b0d78c473c3f0d1e5814ef18e7f07
Marvin Kurland <bugemarvin@outlook.com>
2023-03-10 01:40:31 +0300

    sorts

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,24 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 // finding characters of a movie
 
 const request = require('request');
 request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, req, res) => {
   if (err) console.log(err);
 
-  if (req) {
+  if (req.statusCode === 200) {
     const info = JSON.parse(res).characters;
-
-    for (const people of Object.keys(info)) {
-      request(info[people], 0);
-      request(info[people], (err1, req1, res1) => {
-        if (err1) {
-          console.log(err1);
-        }
-
-        if (req1) {
-          const character = JSON.parse(res1).name;
-          console.log(character);
-        }
-      });
-    }
+    sortUsers(info, 0);
   }
 });
+
+function sortUsers (info, counts) {
+  request(info[counts], (err1, req1, res1) => {
+    if (err1) console.log(err1);
+
+    if (req1.statusCode === 200) {
+      const user = JSON.parse(res1).name;
+      console.log(user)
+      sortUsers(info, counts + 1);
+    }
+  });
+};
