e0449586fe69f109e1e21246609a0c0417b4cf8c
Marvin Kurland <bugemarvin@outlook.com>
2023-03-13 01:05:30 +0300

    added

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -11,11 +11,13 @@ function myRequster () {
       }
       if (req.statusCode === 200) {
         const users = JSON.parse(res).characters;
-        for (let i = 0; i < users.length; i++) {
-          request.get(users[i], (err1, req1, res1) => {
+        for (let i = 1; i <= users.length; i++) {
+          request.get('https://swapi-api.alx-tools.com/api/people/' + i, async (err1, req1, res1) => {
             if (err1) reject(console.log(err1));
             if (req1.statusCode === 200) {
-              resolve(console.log(JSON.parse(res1).name));
+              if (users[i]) {
+                resolve(console.log(await JSON.parse(res1).name));
+              }
             }
           });
         }
