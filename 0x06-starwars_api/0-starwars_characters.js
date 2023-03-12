6aa3b893c64a9028e4cc19130596da5210e5310b
Marvin Kurland <bugemarvin@outlook.com>
2023-03-13 00:59:56 +0300

    rechecking

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,27 @@
 #!/usr/bin/node
 // finding characters of a movie
-
-const { info } = require('console');
 const request = require('request');
-request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], async (err, req, res) => {
-  if (err) throw err;
-
-  if (req.statusCode === 200) {
-    const info = JSON.parse(res).characters;
-    await sortOrder(info, 0);
-  }
-});
-
-function sortOrder (info, character) {
-  for (character of info) {
-    request.get(character, async (err1, req1, res1) => {
-      if (err1) throw err1;
-      if (req1.statusCode === 200) {
-        const user = JSON.parse(res1).name;
-        await console.log(user);
+const myUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const myInputs = process.argv[2];
+function myRequster () {
+  request.get(myUrl + myInputs, (err, req, res) => {
+    return new Promise((resolve, reject) => {
+      if (err) {
+        reject(err);
+      }
+      if (req.statusCode === 200) {
+        const users = JSON.parse(res).characters;
+        for (let i = 0; i < users.length; i++) {
+          request.get(users[i], (err1, req1, res1) => {
+            if (err1) reject(console.log(err1));
+            if (req1.statusCode === 200) {
+              resolve(console.log(JSON.parse(res1).name));
+            }
+          });
+        }
       }
     });
-  }
+  });
 }
+
+myRequster();
