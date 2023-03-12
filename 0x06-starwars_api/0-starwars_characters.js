1422519225b97901fd8c9acab221083eb2ccb6f3
Marvin Kurland <bugemarvin@outlook.com>
2023-03-12 22:57:07 +0300

    try checking

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,25 @@
 #!/usr/bin/node
 // finding characters of a movie
 
+const { info } = require('console');
 const request = require('request');
-request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, req, res) => {
-  if (err) console.log(err);
+request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], async (err, req, res) => {
+  if (err) throw err;
 
   if (req.statusCode === 200) {
     const info = JSON.parse(res).characters;
-    sortUsers(info, 0);
+    await sortOrder(info, 0);
   }
 });
 
-function sortUsers (info, counts) {
-  request(info[counts], (err1, req1, res1) => {
-    if (err1) console.log(err1);
-
-    if (req1.statusCode === 200) {
-      const user = JSON.parse(res1).name;
-      console.log(user);
-      sortUsers(info, counts + 1);
-    }
-  });
+function sortOrder (info, character) {
+  for (character of info) {
+    request.get(character, async (err1, req1, res1) => {
+      if (err1) throw err1;
+      if (req1.statusCode === 200) {
+        const user = JSON.parse(res1).name;
+        await console.log(user);
+      }
+    });
+  }
 }
