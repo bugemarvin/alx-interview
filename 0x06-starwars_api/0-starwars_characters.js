c03d27b0997e0a70f54317042af94dc01af0efca
Marvin Kurland <bugemarvin@outlook.com>
2023-03-13 08:38:44 +0300

    checks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,13 +4,13 @@ const request = require('request');
 const myUrl = 'https://swapi-api.alx-tools.com/api/films/';
 const myInputs = process.argv[2];
 function myRequster () {
-  request.get(myUrl + myInputs, (err, req, res) => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    request.get(myUrl + myInputs, async (err, req, res) => {
       if (err) {
         reject(err);
       }
       if (req.statusCode === 200) {
-        const users = JSON.parse(res).characters;
+        const users = await JSON.parse(res).characters;
         resolve(characters(users, 0));
       }
     });
@@ -18,11 +18,12 @@ function myRequster () {
 }
 
 function characters (info, j) {
-  for (; j < info.length; j++) {
+  if (j < info.length) {
     request.get(info[j], async (err1, req1, res1) => {
       if (err1) console.log(err1);
       if (req1.statusCode === 200) {
         console.log(await JSON.parse(res1).name);
+        characters(info, j + 1);
       }
     });
   }
