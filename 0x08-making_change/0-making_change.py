d9f4122a3838524030b008b831ff92bc50f6723a
Marvin Kurland <bugemarvin@outlook.com>
2023-03-25 11:29:47 +0300

    refactor

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,7 +3,6 @@
     determine the fewest number of coins needed to meet
     a given amount total.
 '''
-import sys
 
 
 def makeChange(coins, total):
@@ -12,17 +11,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    rem = total
+
     coins_count = 0
-    coin_idx = 0
     sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
-            return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
+
+    for coin in sorted_coins:
+        while total >= coin:
+            total -= coin
             coins_count += 1
-        else:
-            coin_idx += 1
+
+    if total > 0:
+        return -1
+
     return coins_count
