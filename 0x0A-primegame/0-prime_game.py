bcf47ffd1c356a42b0edec8473e90819d929995f
Marvin Kurland <bugemarvin@outlook.com>
2023-04-07 00:39:10 +0300

    adding rounds

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,8 +13,8 @@ def isWinner(x, nums):
     """_isWinner_
 
     Args:
-        x (integer): _description_
-        nums (array): _description_
+        x (integer): number of rounds
+        nums (array): numbers to choosed from
 
     Returns:
         None: if winner is not found
@@ -22,33 +22,34 @@ def isWinner(x, nums):
     """
     marias_wins = 0
     bens_wins = 0
-    for n in nums:
-        # initialize the set of available numbers
-        nums_set = set(range(2, n+1))
+    for _ in range(x):
+        for n in nums:
+            # initialize the set of available numbers
+            nums_set = set(range(2, n+1))
 
-        # simulate the game
-        marias_turn = True
-        while nums_set:
-            # find the smallest prime number
-            p = min(filter(lambda x: all(x %
-                    i != 0 for i in range(2, int(x**0.5)+1)), nums_set))
+            # simulate the game
+            marias_turn = True
+            while nums_set:
+                # find the smallest prime number
+                p = min(filter(lambda x: all(x %
+                        i != 0 for i in range(2, int(x**0.5)+1)), nums_set))
 
-            # remove p and its multiples from the set
-            nums_set.difference_update(range(p, n+1, p))
+                # remove p and its multiples from the set
+                nums_set.difference_update(range(p, n+1, p))
 
-            # switch turns
-            marias_turn = not marias_turn
+                # switch turns
+                marias_turn = not marias_turn
 
-        # determine the winner
-        if marias_turn:
-            bens_wins += 1
+            # determine the winner
+            if marias_turn:
+                bens_wins += 1
+            else:
+                marias_wins += 1
+
+        # determine the overall winner
+        if marias_wins > bens_wins:
+            return "Maria"
+        elif bens_wins > marias_wins:
+            return "Ben"
         else:
-            marias_wins += 1
-
-    # determine the overall winner
-    if marias_wins > bens_wins:
-        return "Maria"
-    elif bens_wins > marias_wins:
-        return "Ben"
-    else:
-        return None
+            return None
