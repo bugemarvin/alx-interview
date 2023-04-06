1e5526f8ee25eea5ea634563a57639cfacb75052
Marvin Kurland <bugemarvin@outlook.com>
2023-04-06 23:19:59 +0300

    pep8 styling fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -22,17 +22,18 @@ def isWinner(x, nums):
     """
     maria_wins = 0
     ben_wins = 0
-    for n in nums:
-        nums_set = set(range(2, n+1))
-        while nums_set:
-            p = min(filter(lambda x: all(x %
-                    i != 0 for i in range(2, int(x**0.5)+1)), nums_set))
-            nums_set -= set(range(p, n+1, p))
-            maria_turn = None
-            maria_turn = not maria_turn
-            if maria_turn:
-                ben_wins += 1
-            else:
-                maria_wins += 1
+    for _ in range(x):
+        for n in nums:
+            nums_set = set(range(2, n+1))
+            while nums_set:
+                p = min(filter(lambda x: all(x %
+                        i != 0 for i in range(2, int(x**0.5)+1)), nums_set))
+                nums_set -= set(range(p, n+1, p))
+                maria_turn = None
+                maria_turn = not maria_turn
+                if maria_turn:
+                    ben_wins += 1
+                else:
+                    maria_wins += 1
         return "Maria" if maria_wins > ben_wins else "Ben"\
             if ben_wins > maria_wins else None
